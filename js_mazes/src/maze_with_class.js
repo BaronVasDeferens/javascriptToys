771d74e9d51cf92be30a6dc57f27d8319a718eb3
skot <baronvasdeferens@gmail.com>
2018-04-02 23:48:46 -0700

    Works. Works better than before, but need to use direct-access to check inclusion in master maze[][] array rather than exhaustive check for inclusion in list

--- js_mazes/src/maze_with_class.js
+++ js_mazes/src/maze_with_class.js
@@ -1,10 +1,10 @@
     // CONSTANTS
-    const mazeRows = 30;
-    const mazeCols = 30;
+    const mazeRows = 40;
+    const mazeCols = 40;
     const roomSize = 10;
 
 	const canvasWidth = 1100
-	const canvasHeight = 700;
+	const canvasHeight = 800;
 
     const millisecondDelay = 1;
 
@@ -23,7 +23,13 @@
 
     class Edge {
 
-        constructor(v1, v2) {
+        constructor(row, col) {
+            this.row = row,
+            this.col = col;
+            this.open = false;
+        }
+
+        setVertexes(v1, v2) {
             this.v1 = v1;
             this.v2 = v2;
         }
@@ -31,12 +37,12 @@
         display() {
             return this.v1.display() + " <-> " + this.v2.display();
         }
+
+        setToOpen() {
+            this.open = true;
+        }
     }
 
-
-    var canvasDef = "<canvas id=\"myCanvas\" width=\"%WIDTH%\" height=\"%HEIGHT%\"></canvas>";
-    canvasDef = canvasDef.replace("%WIDTH%", canvasWidth);
-    canvasDef = canvasDef.replace("%HEIGHT%", canvasHeight);
 
 	var mazeArray = new Array(mazeRows);
 	var allRooms = new Array();
@@ -45,196 +51,140 @@
     var connections = new Array();
     var frontier = new Array();     // set of edges where x in inMaze and y is not in inMaze
 
-    var finalMaze = new Array(mazeRows * 2 - 1);
-    for (var i = 0; i < mazeCols; i++) {
-        finalMaze[i] = new Array(mazeCols * 2 - 1);
-    }
+    var finalMaze = new Array();
+    var finalMazeRows = (mazeRows * 2) - 1;
+    var finalMazeCols = (mazeCols * 2) - 1;
 
-	// Setup (IFFE function)
-	// Insert a drawable canvas element into the page
 
-	var setup = function() {
 
-        var mazeArea = document.getElementById('mazeArea');
-        mazeArea.innerHTML = canvasDef
 
-        console.log("Creating rooms...");
-		for (var i = 0; i < mazeRows; i++) {
+    var newSetup = function() {
 
-			mazeArray[i] = new Array(mazeCols);
+        insertHtmlElements();
 
-			for (var j = 0; j < mazeCols; j++) {
+        console.log("finalMaze rows / cols: " + finalMazeRows + " / " + finalMazeCols);
 
-				var room = new Room(i,j);
+        finalMaze = new Array(finalMazeRows);
 
-                room.edges = new Array();
+        // First pass: create the rooms and edges
+        for (var i = 0; i < finalMazeRows; i++) {
 
-				allRooms.push(room);
-				mazeArray[i][j] = room;
-			}
-		}
+            finalMaze[i] = new Array(finalMazeCols);
 
-        console.log("Creating edges...");
+            for (var j = 0; j < finalMazeCols; j++) {
 
-        // Create list of edges
-
-        for (var i = 0; i < mazeRows; i++) {
-
-            for (var j = 0; j < mazeCols; j++) {
-
-                var currentRoom = mazeArray[i][j];
-
-                var up = getRoom(currentRoom.row - 1, currentRoom.col);
-                if (up !== undefined) {
-                        var e = new Edge(currentRoom, up);
-                        edges.push(e);
-                        currentRoom.edges.push(e);
-                }
-
-                var down = getRoom(currentRoom.row + 1, currentRoom.col);
-                if (down !== undefined) {
-                        var e = new Edge(currentRoom, down);
-                        edges.push(e);
-                        currentRoom.edges.push(e);
-                }
-
-                var right = getRoom(currentRoom.row, currentRoom.col + 1);
-                if (right !== undefined) {
-                        var e = new Edge(currentRoom, right);
-                        edges.push(e);
-                        currentRoom.edges.push(e);
-                }
-
-                 var left = getRoom(currentRoom.row, currentRoom.col - 1);
-                if (left !== undefined) {
-                    var e = new Edge(currentRoom, left);
-                    edges.push(e);
-                    currentRoom.edges.push(e);
+                if ((i % 2 == 0) && (j % 2 == 0)) {
+                    var room = new Room(i,j);
+                    allRooms.push(room);
+                    finalMaze[i][j] = room;
+                } else if ((i % 2 == 0) ^ (j % 2 == 0)) {
+                    var edge = new Edge(i,j);
+                    finalMaze[i][j] = edge;
                 }
 
             }
-
         }
 
+        // Second pass: connect edges (and add edges to the master list)
+        for (var i = 0; i < finalMazeRows; i++) {
 
-        // Select initial vertex (room)
+            for (var j = 0; j < finalMazeCols; j++) {
+
+                var edge = finalMaze[i][j];
+
+                if (edge === undefined) {
+                    continue;
+                }
+
+                if (! edge instanceof Edge) {
+                    continue;
+                }
+
+                if (i % 2 != 0) {
+                    var up = getCell(edge.row - 1, edge.col);
+                    var down = getCell(edge.row + 1, edge.col);
+                    if (up instanceof Room && down instanceof Room) {
+                        edge.setVertexes(up, down);
+                        edges.push(edge);
+                    }
+                } else if (j % 2 != 0) {
+                    var left = getCell(edge.row, edge.col - 1);
+                    var right = getCell(edge.row, edge.col + 1);
+                    if (left instanceof Room && right instanceof Room) {
+                        edge.setVertexes(right, left);
+                        edges.push(edge);
+                    }
+                }
+
+            }
+        }
+
+        // Put initial room into maze
         var startRoom = getRandomRoom();
+        console.log("START : " + startRoom.display());
         inMaze.push(startRoom);
-
-        startRoom.edges.forEach(function (r) {
-            frontier.push(r);
+        getAdjacentEdges(startRoom.row, startRoom.col).forEach ( function (edge) {
+            frontier.push(edge);
         });
+
+        drawFinalMaze();
 
         var interval = window.setInterval(
             function () {
                 if (frontier.length > 0) {
-                    createMaze()
+                    createMaze();
+                    drawFinalMaze();
                 }
                 else {
                     clearInterval(interval)
-                    console.log("ALL DONE!")
+                    console.log("ALL DONE!");
                 }
             }
         , millisecondDelay);
 
-	}();
 
-    function printRoom(room) {
-        return "(" + room.row + "," + room.col + ")";
+
+    }();
+
+    function insertHtmlElements() {
+        console.log("Inserting HTML...");
+        var canvasDef = "<canvas id=\"myCanvas\" width=\"%WIDTH%\" height=\"%HEIGHT%\"></canvas>";
+        canvasDef = canvasDef.replace("%WIDTH%", canvasWidth);
+        canvasDef = canvasDef.replace("%HEIGHT%", canvasHeight);
+        document.getElementById('mazeArea').innerHTML = canvasDef;
     }
 
+    function getAdjacentEdges(row, col) {
 
+        var room = getCell(row, col);
+        var adjacentRooms = new Array();
 
-    function createMaze() {
-
-        if (frontier.length <= 0) {
-            console.log("DONE");
+        var up = getCell(row, col-1);
+        if (up instanceof Edge) {
+            adjacentRooms.push(up);
         }
 
-        frontier = shuffleArray(frontier);
-        var edge = frontier.pop();
-
-        if (!inMaze.includes(edge.v2)) {
-            inMaze.push(edge.v2);
-            connections.push(edge);
-            edge.v2.edges.forEach( function (e) {
-                if (!inMaze.includes(e.v2)) {
-                    frontier.push(e);
-                }
-            })
-
-            drawBigMaze();
+        var down = getCell(row, col+1);
+        if (down instanceof Edge) {
+            adjacentRooms.push(down);
         }
 
-        else {
-//            console.log("."); // 361 extra edges :(
+        var left = getCell(row-1, col);
+        if (left instanceof Edge) {
+            adjacentRooms.push(left);
         }
+
+        var right = getCell(row+1, col);
+        if (right instanceof Edge) {
+            adjacentRooms.push(right);
+        }
+
+        return adjacentRooms;
     }
 
-
-	function drawBigMaze() {
-
-	    var canvas = document.getElementById("myCanvas");
-        var context = canvas.getContext("2d");
-
-        context.fillStyle = "#000000"; // black
-        context.fillRect(0,0,canvasWidth,canvasHeight);
-
-        context.fillStyle ="#FFFFFF"; // white
-
-        inMaze.forEach(function (room) {
-                context.fillRect(
-                    room.col * 2 * roomSize,
-                    room.row * 2 * roomSize,
-                    roomSize,
-                    roomSize);
-        });
-
-        context.fillStyle = "#FFFFFF";
-
-        connections.forEach(function (vtx) {
-            var v1, v2, ox, oy;
-
-            // v1 to the left
-            if (vtx.v1.col < vtx.v2.col) {
-                v1 = vtx.v1;
-                v2 = vtx.v2;
-
-                ox = v1.col * 2 * roomSize + roomSize;
-                oy = v1.row * 2 * roomSize;
-
-            }
-            // v1 on top
-            else if (vtx.v1.row < vtx.v2.row) {
-              v1 = vtx.v1;
-              v2 = vtx.v2;
-              ox = v1.col * 2 * roomSize;
-              oy = v1.row * 2 * roomSize + roomSize;
-            }
-
-            else if (vtx.v1.col == vtx.v2.col) {
-                    v1 = vtx.v2;
-                    v2 = vtx.v1;
-                    ox = v1.col * 2 * roomSize ;
-                    oy = v1.row * 2 * roomSize + roomSize;
-            }
-            else {
-                v1 = vtx.v2;
-                v2 = vtx.v1;
-                ox = v1.col * 2 * roomSize +roomSize;
-                oy = v1.row * 2 * roomSize ;
-            }
-
-            context.fillRect(ox, oy, roomSize, roomSize);
-
-        });
-
-
-	}
-
-    function getRoom(row, col) {
+    function getCell(row, col) {
         try {
-            return mazeArray[row][col];
+            return finalMaze[row][col];
         } catch (e) {
             return undefined;
         }
@@ -257,4 +207,69 @@
     function getRandomRoom() {
         var index = Math.floor(Math.random() * 1000 % (mazeRows * mazeCols));
         return allRooms[index]
+    }
+
+    function createMaze() {
+
+            if (frontier.length <= 0) {
+                console.log("DONE");
+            }
+
+            frontier = shuffleArray(frontier);
+            var edge = frontier.pop();
+
+//            console.log("chose " + edge.display());
+
+            var room;
+
+            if (!inMaze.includes(edge.v1)) {
+                room = edge.v1;
+                inMaze.push(edge);
+                inMaze.push(room);
+                getAdjacentEdges(room.row, room.col).forEach( function (e) {
+                    if (!frontier.includes(e)) {
+                        frontier.push(e);
+                    }
+                });
+            } else if (!inMaze.includes(edge.v2)) {
+                room = edge.v2;
+                inMaze.push(edge);
+                inMaze.push(room);
+                getAdjacentEdges(room.row, room.col).forEach( function (e) {
+                    if (!frontier.includes(e)) {
+                        frontier.push(e);
+                    }
+                });
+            }
+        }
+
+    function drawFinalMaze() {
+
+        var canvas = document.getElementById("myCanvas");
+        var context = canvas.getContext("2d");
+
+        context.fillStyle = "#000000"; // black
+        context.fillRect(0,0,canvasWidth,canvasHeight);
+
+        context.fillStyle ="#FF0000"; // red
+        frontier.forEach(function (cell) {
+
+            context.fillRect(
+                cell.col * roomSize,
+                cell.row * roomSize,
+                roomSize,
+                roomSize);
+        });
+
+        context.fillStyle ="#FFFFFF"; // white
+        inMaze.forEach(function (cell) {
+
+            context.fillRect(
+                cell.col * roomSize,
+                cell.row * roomSize,
+                roomSize,
+                roomSize);
+        });
+
+
     }

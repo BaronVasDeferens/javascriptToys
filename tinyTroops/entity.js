4c6e71d577ed894b25b4f554fc76e814c803647b
Skot <baronvasdeferens@gmail.com>
2021-06-27 18:34:23 -0700

    basic unit selction

--- tinyTroops/entity.js
+++ tinyTroops/entity.js
@@ -13,6 +13,8 @@ export class Dot extends Entity {
     size = 50;
     color = "#FF0000";
 
+    lineWidth = 10;
+
     constructor(x,y,size, color) {
         super();
         this.centerX = x;
@@ -24,6 +26,7 @@ export class Dot extends Entity {
     render(context) {
 
         context.strokeStyle = this.color;
+        context.lineWidth = this.lineWidth;
         context.beginPath();
 
         context.ellipse(this.centerX, this.centerY, this.size, this.size, 2 * Math.PI, 2 * Math.PI, false);
@@ -33,6 +36,93 @@ export class Dot extends Entity {
         //     this.y + Math.sin((this.orientation - 90) * Math.PI / 180) * lineLength);
         context.stroke();
 
+    }
+}
+
+export class Line extends Entity {
+
+    startX = 0;
+    startY = 0;
+    endX = 0;
+    endY = 0;
+    color = "#FF0000"
+    lineWidth = 1;
+
+    constructor(x1, y1, x2, y2, lineWidth, color) {
+        super();
+        this.startX = x1;
+        this.startY = y1;
+        this.endX =x2;
+        this.endY =y2;
+        this.color = color;
+        this.lineWidth = lineWidth;
+    }
+
+    render(context) {
+        context.strokeStyle = this.color;
+        context.lineWidth = this.lineWidth;
+
+        context.save();
+        context.beginPath();
+        context.moveTo(this.startX, this.startY);
+        context.lineTo(this.endX, this.endY);
+        context.stroke();
+        context.restore();
+    }
+
+
+}
+
+
+export class Soldier extends Entity {
+
+    x = 0;
+    y = 0;
+    image = new Image();
+
+    constructor(x,y) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.image.src = "resources/guy_1.png";
+    }
+
+    /**
+     * Returns the coordinates of this entity if the click was within this image's bounds;
+     * 
+     * @param {*} event 
+     */
+    isClicked(event) {
+
+        if (event.x >= this.x - (this.image.width / 2) && event.x <= this.x + (this.image.width / 2) ) {
+            if (event.y >= this.y - (this.image.height / 2) && event.y <= this.y + ( this.image.height /2)) {
+                return { x: this.x, y: this.y };
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    getCenteredCoords() {
+        return {
+            x: this.x, // + (this.image.width / 2),
+            y: this.y // + (this.image.height / 2)
+        }
+    }
+
+    updatePositionByDelta(dx, dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    // Draws CENTERED on x,y
+    render(context) {
+        context.save();
+        context.translate(this.x, this.y);
+        context.drawImage(this.image, -(this.image.width / 2), - (this.image.height / 2));
+        context.restore();
     }
 
 }
--- tinyTroops/tiny_troops.js
+++ tinyTroops/tiny_troops.js
@@ -5,7 +5,8 @@
  * And if THAT doesn't work (and you use BASH), try: "sudo npm install --global http-server"
  */
 
-import { Dot } from './entity.js';
+import { Dot, Line } from './entity.js';
+import { Soldier } from './entity.js';
 
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
@@ -18,19 +19,35 @@ canvas.height = innerHeight;
  * GAME STATES
  * 
  */
-var States = Object.freeze({IDLE: 0, UNIT_SELECTED: 1});
+var States = Object.freeze({IDLE: "IDLE", UNIT_SELECTED: "UNIT_SELECTED"});
 var currentState = States.IDLE;
 
-const entities = [];
+var selectedUnitPrimary = null;
+const entitiesResident = [];    // All "permanent" entities (playhers, enemies)
+const entitiesTemporary = [];   // Temporary entities
+const entitiesTransient = [];   // These are cleared after ever render
 
 var setup = function () {
-
     // Include any initiatlization code here
     console.log(">>> Starting...");
+    entitiesResident.push(new Soldier(50,50));
+    entitiesResident.push(new Soldier(150,50));
+    entitiesResident.push(new Soldier(250,50));
+    entitiesResident.push(new Soldier(350,50));
+    entitiesResident.push(new Soldier(450,50));
+
+
+
     beginGame();
 }();
 
 
+
+//Prevent the right click from summoning the context menu
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+
+// Process mouse clicks
 window.onmousedown = function(event) {
 
     switch (currentState) {
@@ -41,24 +58,71 @@ window.onmousedown = function(event) {
                 case 0: 
                     activateAtMouse(event);
                     break;
+                // Right click
                 case 2:
+                    currentState = States.IDLE
+                    entitiesTemporary.length = 0;
                     break;
                 default:
                     break;
             }
             break;
         case States.UNIT_SELECTED:
-        
+
+            switch (event.button) {
+                // left click
+                case 0: 
+                    // Preform an action
+
+                    break;
+                // Right click
+                case 2:
+                    currentState = States.IDLE
+                    entitiesTemporary.length = 0;
+                    selectedUnitPrimary = null;
+                    break;
+                default:
+                    break;
+            }
+
             break;
         default:
             break;
     }
+
+    console.log("State is now " + currentState);
+
 }
+
+// Process mouse movement
+window.onmousemove = function(event) {
+
+    switch (currentState) {
+        case States.IDLE:
+            break;
+        case States.UNIT_SELECTED:
+            console.log("!! == " + event);
+            let centeredCoords = selectedUnitPrimary.getCenteredCoords();
+            entitiesTransient.push(new Line(centeredCoords.x, centeredCoords.y, event.x, event.y, 5, "#FF0000"));
+            break;
+    }
+
+};
 
 function activateAtMouse(event) {
 
-    var dot = new Dot(event.x, event.y, 50, "#FF0000");
-    entities.push(dot);
+    entitiesResident.forEach (resident => {
+        // LOk for a unit under this click
+        let centeredOnClick = resident.isClicked(event);
+        if (centeredOnClick) {
+            var dot = new Dot(centeredOnClick.x, centeredOnClick.y, 50, "#000000");
+            entitiesTemporary.push(dot);
+            selectedUnitPrimary = resident;
+            currentState = States.UNIT_SELECTED;
+        }
+    });
+
+
 
 }
 
@@ -82,7 +146,8 @@ function processPlayerInput() {
 }
 
 function drawGrid(context, size) {
-    context.strokeStyle = "#000000";
+    context.strokeStyle = "#a8a8a8";
+    context.lineWidth = 1;
     for (var i = 0; i < size; i++) {
         for (var j = 0; j < size; j++) {
             context.strokeRect(i * size, j * size, size, size);
@@ -93,11 +158,22 @@ function drawGrid(context, size) {
 function drawScene() {
     // Draw background
     context.fillStyle = "#b8bab9";
+    context.lineWidth =
     context.fillRect(0, 0, canvas.width, canvas.height);
     drawGrid(context, 50);
 
-    entities.forEach( entity => { 
+    entitiesResident.forEach( entity => {
         entity.render(context);
     });
+
+    entitiesTemporary.forEach( entity => { 
+        entity.render(context);
+    });
+
+    entitiesTransient.forEach( entity => { 
+        entity.render(context);
+    });
+
+    entitiesTransient.length = 0;
 
 }

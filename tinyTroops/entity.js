00c341e3b26229f49d226e0984e34d87acf17b59
West <swest@leanpath.com>
2022-08-18 15:23:03 -0700

    new art, kinda

--- tinyTroops/entity.js
+++ tinyTroops/entity.js
@@ -401,7 +401,7 @@ export class Blob extends Entity {
 
     constructor(id, x, y) {
         super(id, x, y);
-        this.imageAlive.src = "resources/blob_strip_2.png";
+        this.imageAlive.src = "resources/blob_new_strip.png";
         this.imageDead.src = "resources/blob_dead_1.png";
     }
 

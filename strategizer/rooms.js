c439d6c36c98831eb7a0e292195412b0c439e350
Scott West <baronvasdeferens@gmail.com>
2025-08-18 20:20:31 -0700

    Basic rooms

---
+++ strategizer/rooms.js
@@ -0,0 +1,233 @@
+export const Directions = Object.freeze({
+    UP: "UP",
+    DOWN: "DOWN",
+    LEFT: "LEFT",
+    RIGHT: "RIGHT"
+});
+
+export class Border {
+
+    constructor(xStart, yStart, width, height) {
+
+        this.xStart = xStart;
+        this.yStart = yStart;
+        this.width = width;
+        this.height = height;
+
+        this.xEnd = this.xStart + width;
+        this.yEnd = this.yStart + height;
+    }
+
+    render(context) {
+        context.fillStyle = "#000000"
+        context.fillRect(this.xStart, this.yStart, this.width, this.height);
+    }
+
+}
+
+export class Room {
+
+    x = 0;
+    y = 0;
+    color = "#000000";
+
+    borders = new Array();
+
+    upOpen = false;
+    downOpen = false;
+    leftOpen = false;
+    rightOpen = false;
+
+    constructor(x, y, size, color) {
+
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.wallWidth = this.size * 0.02;
+
+        this.changeColor("#FFFFFF");
+    }
+
+    changeColor(newColor) {
+        if (newColor == null) {
+            this.color = this.getRandomColor();
+        } else {
+            this.color = newColor;
+        }
+    }
+
+    isOpen() {
+        return (this.upOpen || this.downOpen || this.leftOpen || this.rightOpen);
+    }
+
+    isBorderCollision(click) {
+        return this.borders.some(border => {
+            return click.offsetX >= border.xStart && click.offsetX <= border.xEnd && click.offsetY >= border.yStart && click.offsetY <= border.yEnd
+        })
+    }
+
+    render(context) {
+
+        // Draw the base color
+        context.fillStyle = this.color;
+        context.fillRect(this.x * this.size, this.y * this.size, this.size, this.size);
+
+        // Draw the borders
+        this.borders.forEach(border => {
+            border.render(context);
+        });
+    }
+
+    computeBorders() {
+
+        // TODO: clear existing borders?
+
+        if (this.upOpen) {
+            this.borders.push(new Border(this.x * this.size, this.y * this.size, this.size / 3, this.wallWidth));
+            this.borders.push(new Border((this.x * this.size) + (2 / 3 * this.size), this.y * this.size, this.size / 3, this.wallWidth));
+        } else {
+            this.borders.push(new Border(this.x * this.size, this.y * this.size, this.size, this.wallWidth));
+        }
+
+        if (this.rightOpen) {
+            this.borders.push(new Border((this.x * this.size) + this.size - this.wallWidth, (this.y * this.size), this.wallWidth, this.size / 3));
+            this.borders.push(new Border((this.x * this.size) + this.size - this.wallWidth, (this.y * this.size) + ((2 / 3) * this.size), this.wallWidth, this.size / 3));
+        } else {
+            this.borders.push(new Border((this.x * this.size) + this.size - this.wallWidth, (this.y * this.size), this.wallWidth, this.size));
+        }
+
+        if (this.downOpen) {
+            this.borders.push(new Border(this.x * this.size, (this.y * this.size) + this.size - this.wallWidth, this.size / 3, this.wallWidth));
+            this.borders.push(new Border((this.x * this.size) + (2 / 3 * this.size), (this.y * this.size) + this.size - this.wallWidth, this.size / 3, this.wallWidth));
+        } else {
+            this.borders.push(new Border(this.x * this.size, (this.y * this.size) + this.size - this.wallWidth, this.size, this.wallWidth));
+        }
+
+        if (this.leftOpen) {
+            this.borders.push(new Border((this.x * this.size), this.y * this.size, this.wallWidth, this.size / 3));
+            this.borders.push(new Border((this.x * this.size), (this.y * this.size) + (2 / 3 * this.size), this.wallWidth, this.size / 3));
+        } else {
+            this.borders.push(new Border((this.x * this.size), this.y * this.size, this.wallWidth, this.size));
+        }
+    }
+
+    getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+}
+
+export class Maze {
+
+    rooms = new Array();
+
+    constructor(numRows, numCols, roomSize) {
+
+        this.numRows = numRows;
+        this.numCols = numCols;
+        this.roomSize = roomSize;
+
+        for (var i = 0; i < numCols; i++) {
+            for (var j = 0; j < numRows; j++) {
+                this.rooms.push(
+                    new Room(
+                        i, j, roomSize
+                    )
+                )
+            }
+        }
+    }
+
+    computeBorders() {
+        this.rooms.forEach(room => {
+            room.computeBorders();
+        });
+    }
+
+    render(context) {
+        this.rooms.forEach(room => {
+            room.render(context);
+        });
+    }
+
+    findRoom(x, y) {
+        return this.rooms.filter(room => { return (room.x == x && room.y == y) })[0];
+    }
+
+    getRoomAtClick(click) {
+        var xClick = click.offsetX;
+        var yClick = click.offsetY;
+
+        return this.rooms.filter(room => {
+            return (xClick >= (room.x * room.size))
+                && (xClick <= (room.x * room.size) + room.size)
+                && (yClick >= room.y * room.size)
+                && (yClick <= (room.y * room.size) + room.size)
+        })[0];
+    }
+
+    openNeighboringRooms(x, y, dir) {
+
+        var targetRoom = this.findRoom(x, y);
+
+        if (targetRoom != null) {
+
+            this.openRoomSingle(targetRoom, dir);
+
+            // Find the neighbor and open it
+            var neighborRoom;
+
+            switch (dir) {
+                case Directions.UP:
+                    neighborRoom = this.rooms.filter(room => { return (room.x == x && room.y == y - 1) })[0];
+                    this.openRoomSingle(neighborRoom, Directions.DOWN);
+                    break;
+                case Directions.DOWN:
+                    neighborRoom = this.rooms.filter(room => { return (room.x == x && room.y == y + 1) })[0];
+                    this.openRoomSingle(neighborRoom, Directions.UP);
+                    break;
+                case Directions.LEFT:
+                    neighborRoom = this.rooms.filter(room => { return (room.x == x - 1 && room.y == y) })[0];
+                    this.openRoomSingle(neighborRoom, Directions.RIGHT);
+                    break;
+                case Directions.RIGHT:
+                    neighborRoom = this.rooms.filter(room => { return (room.x == x + 1 && room.y == y) })[0];
+                    this.openRoomSingle(neighborRoom, Directions.LEFT);
+                    break;
+            }
+
+        } else {
+            console.log(`ERROR : openNeighboringRooms : room ${x} ${y} not found!`)
+        }
+    }
+
+    openRoomSingle(targetRoom, dir) {
+
+        if (targetRoom == null) {
+            console.log(`ERROR: openRoomSingle : targetRoom not found!`);
+            return;
+        }
+
+        console.log(`opening room ${targetRoom.x} ${targetRoom.y} -> ${dir}`);
+
+        switch (dir) {
+            case Directions.UP:
+                targetRoom.upOpen = true;
+                break;
+            case Directions.DOWN:
+                targetRoom.downOpen = true;
+                break;
+            case Directions.LEFT:
+                targetRoom.leftOpen = true;
+                break;
+            case Directions.RIGHT:
+                targetRoom.rightOpen = true;
+                break;
+        }
+    }
+}
--- strategizer/strategizer.js
+++ strategizer/strategizer.js
@@ -1,7 +1,55 @@
+import { Maze, Directions } from "./rooms.js";
+
 const canvas = document.getElementById('playArea');
 const context = canvas.getContext('2d');
+
+const numRows = 5;
+const numCols = 5;
+const roomSize = canvas.width / numCols;
+
+const maze = new Maze(numRows, numCols, roomSize);
 
 var setup = function () {
     context.fillStyle = "#FF0000";
     context.fillRect(0, 0, canvas.width, canvas.height);
+
+
+    maze.openNeighboringRooms(0, 0, Directions.RIGHT);
+    maze.openNeighboringRooms(0, 0, Directions.DOWN);
+    maze.openNeighboringRooms(1, 0, Directions.RIGHT);
+    maze.openNeighboringRooms(3, 0, Directions.DOWN);
+    maze.openNeighboringRooms(0, 1, Directions.DOWN);
+    maze.openNeighboringRooms(1, 1, Directions.LEFT);
+    maze.openNeighboringRooms(1, 1, Directions.DOWN);
+    maze.openNeighboringRooms(2, 1, Directions.UP);
+    maze.openNeighboringRooms(2, 1, Directions.RIGHT);
+    maze.openNeighboringRooms(3, 1, Directions.DOWN);
+    maze.openNeighboringRooms(0, 2, Directions.RIGHT);
+    maze.openNeighboringRooms(1, 2, Directions.RIGHT);
+    maze.openNeighboringRooms(1, 2, Directions.DOWN);
+    maze.openNeighboringRooms(2, 2, Directions.UP);
+    maze.openNeighboringRooms(2, 2, Directions.DOWN);
+    maze.openNeighboringRooms(3, 2, Directions.DOWN);
+    maze.openNeighboringRooms(0, 3, Directions.RIGHT);
+    maze.openNeighboringRooms(1, 3, Directions.RIGHT);
+    maze.openNeighboringRooms(3, 3, Directions.LEFT);
+    maze.computeBorders();
+
+
+
+    beginGame();
 }();
+
+function beginGame() {
+    updateGameState();
+    render(context);
+    requestAnimationFrame(beginGame);
+}
+
+function updateGameState() {
+
+}
+
+function render(context) {
+    maze.render(context);
+}
